1116827a935bc71d06d2f12ebaa9cdc5cfe83a83
Ethiel <ethiel97@gmail.com>
2025-02-09 05:51:17 -0500

    feat: adjust job og preview

--- components/OgImage/JobOffer.vue
+++ components/OgImage/JobOffer.vue
@@ -6,13 +6,17 @@ import OgPreview from "~/components/job/OgPreview.vue";
 
 const jobOffer = defineProps<Partial<JobOffer>>()
 
+const jobOfferTitle = computed(() => jobOffer.title.length > 85 ? `${jobOffer.title.substring(0, 85)}...` : jobOffer.title)
 </script>
 
 <template>
   <div class="relative">
     <OgPreview/>
-    <p class="absolute top-[75px] left-[300px] text-7xl font-bold !leading-[18px] max-w-xl text-white">
-      {{ jobOffer.title }}
-    </p>
+    <div class="max-w-2xl h-[270px] absolute top-[80px] left-[305px]">
+      <p class="text-7xl font-bold !leading-[20px] text-white">
+        {{ jobOfferTitle }}
+      </p>
+    </div>
+
   </div>
 </template>
